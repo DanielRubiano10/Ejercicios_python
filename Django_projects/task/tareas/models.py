eb7e8c3ee63941af5fc0c1489d407986fea36f27
danielRubiano <danii_95_crm@hotmail.com>
2024-07-26 16:55:35 -0500

    CRUD

--- Django_projects/task/tareas/models.py
+++ Django_projects/task/tareas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Tarea(models.models):
+class Tarea(models.Model):
     id = models.AutoField(primary_key=True) #si la tabla ya tiene una columna auto incremental 
     descripcion = models.CharField(max_length=255)
     completada = models.BooleanField(default=False)
