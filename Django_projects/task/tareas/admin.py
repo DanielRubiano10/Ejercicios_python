8629fb64bd8a9dc5ef497efe0715110db6aaefb7
danielRubiano <danii_95_crm@hotmail.com>
2024-08-08 14:11:31 -0500

    ajustes carpetas

--- Django_projects/task/tareas/admin.py
+++ Django_projects/task/tareas/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Tarea
-=======
-from .models import Tarea 
->>>>>>> dev
 # Register your models here.
 admin.site.register(Tarea)
--- Django_projects/task/tareas/models.py
+++ Django_projects/task/tareas/models.py
@@ -4,16 +4,8 @@ from django.db import models
 class Tarea (models.Model):
     id = models.AutoField(primary_key=True) #campo auto incremental 
     descripcion = models.CharField(max_length=255)
-     
+    completada = models.BooleanField(default=False)
+    
     def __str__(self):
         fila="Titulo: " + self.descripcion + "-" + "Descripcion: " + str(self.completada)
         return fila 
-
-
-
-
-    
-    
-    
-    
-            
